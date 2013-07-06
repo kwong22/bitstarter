70e96b1c0b714c96bf3674879c85dd2f3c1b684c
Kelvin Wong <kwong691@gmail.com>
2013-07-06 01:10:32 +0000

    Modified web.js to read from index.html

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
-
+var fs = require('fs');
 var app = express.createServer(express.logger());
-
+var string = "";
+fs.readFileSync('index.html', function(err, data) {
+    string = data;
+});
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(string);
 });
 
 var port = process.env.PORT || 5000;
