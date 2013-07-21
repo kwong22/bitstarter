cbe90f22b556485408a408d331c058fbe8f3eb49
Kelvin Wong <kwong691@gmail.com>
2013-07-21 21:45:30 +0000

    Modified web.js to show updated index.html automatically

--- web.js
+++ web.js
@@ -1,7 +1,13 @@
 var express = require('express');
 var fs = require('fs');
 var app = express.createServer(express.logger());
+var htmlFile = 'index.html';
 
+app.get('/', function(request, response) {
+    response.send(fs.readFileSync(htmlFile).toString());
+});
+
+/*
 fs.readFile('index.html', 'utf8', function(err, data) {
     if (err) { return console.log(err); }
 
@@ -10,6 +16,7 @@ fs.readFile('index.html', 'utf8', function(err, data) {
     });
 
 });
+*/
 
 var port = process.env.PORT || 8080;
 app.listen(port, function() {
